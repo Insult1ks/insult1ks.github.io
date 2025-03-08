cae5bbfe18a83f3361c837af04bbdcf3583c6064
Insult1k <145651910+Insult1ks@users.noreply.github.com>
2025-03-08 12:59:38 +0700

    Create loader.js

---
+++ js/loader.js
@@ -0,0 +1,34 @@
+function loadScript(src) {
+        return new Promise((resolve, reject) => {
+          const script = document.createElement("script");
+          script.src = src;
+          script.type = "text/javascript";
+
+          script.onload = () => {
+            console.log(`Скрипт ${src} успешно загружен.`);
+            resolve();
+          };
+
+          script.onerror = () => {
+            console.error(`Ошибка загрузки скрипта ${src}.`);
+            reject();
+          };
+
+          document.head.appendChild(script);
+        });
+      }
+
+      // Обработчик нажатия на кнопку
+      document.getElementById("crashButton").addEventListener("click", () => {
+        // Путь к скрипту
+        const scriptUrl = "js/crash.js";
+
+        // Загружаем скрипт
+        loadScript(scriptUrl)
+          .then(() => {
+            console.log("Скрипт загружен и выполнен.");
+          })
+          .catch(() => {
+            console.error("Произошла ошибка при загрузке скрипта.");
+          });
+      });
